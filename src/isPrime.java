f50bec980212a6d53a450dcfc14d89889b604a1b
Candyyao-98 <81028118+Candyyao-98@users.noreply.github.com>
2021-03-22 02:03:53 +1100

    Update isPrime.java

--- src/isPrime.java
+++ src/isPrime.java
@@ -4,6 +4,7 @@ import static java.lang.Math.sqrt;
 
 public class isPrime {
     public static boolean isPrime(int input) {
+        //check if the input number is prime or not
         if (input == 1) return false;
         for (int i = 2; i <= sqrt(input); ++i) {
             if (input % i == 0) return false;
@@ -13,6 +14,7 @@ public class isPrime {
 
     public static void main(String[] args) {
         System.out.println("Enter a number please: ");
+        // Scanner the input number and store as integer type 
         Scanner keyboard = new Scanner(System.in);
         int number = keyboard.nextInt();
         if (number < 0) {
